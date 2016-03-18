d7ec008ab4f867e04d9775d8f3d8c8ecd06437c2
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-18 14:51:48 -0300

    Update mountArch.sh

--- scripts/mountArch.sh
+++ scripts/mountArch.sh
@@ -4,10 +4,3 @@ mkdir /mnt/boot ### para criar a raiz (/mnt) e o boot (/mnt/boot)
 mount /dev/sda2 /mnt/boot ### monta a partição de boot
 mount /dev/mapper/sda3 /mnt ### monta a partição raiz
 arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
-pacman -Syyu
-pacman -S udev --noconfirm ### reinstala o pacote udev - RESOLVE_ERRO(Unable to find root device)
-sed -i '/GRUB_TIMEOUT=/s/5/2/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-mkinitcpio -p linux ### compila a imagem do sistema
-grub-install --recheck /dev/sda
-grub-mkconfig --output /boot/grub/grub.cfg ### configura o grub
