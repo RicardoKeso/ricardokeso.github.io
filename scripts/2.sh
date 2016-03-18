c129d49a1d23738355841b47d71d385be70ca914
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-18 14:54:12 -0300

    Update 2.sh

--- scripts/2.sh
+++ scripts/2.sh
@@ -1,6 +1,6 @@
 
-pacman -Syyu
-pacman -S udev --noconfirm ### reinstala o pacote udev - RESOLVE_ERRO(Unable to find root device)
+#pacman -Syyu
+#pacman -S udev --noconfirm ### reinstala o pacote udev - RESOLVE_ERRO(Unable to find root device)
 sed -i '/GRUB_TIMEOUT=/s/5/2/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 mkinitcpio -p linux ### compila a imagem do sistema
