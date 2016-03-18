449be6d53f697e9df224d3e011c288e307ebac31
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-18 10:28:49 -0300

    Update mountArch.sh

--- scripts/mountArch.sh
+++ scripts/mountArch.sh
@@ -4,3 +4,5 @@ mkdir /mnt/boot ### para criar a raiz (/mnt) e o boot (/mnt/boot)
 mount /dev/sda2 /mnt/boot ### monta a partição de boot
 mount /dev/mapper/sda3 /mnt ### monta a partição raiz
 arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
+sed -i '/GRUB_TIMEOUT=/s/5/2/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
+echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
