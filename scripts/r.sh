ffe496e65d779e8e03120ee0c5461661cd2364ae
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-19 12:36:11 -0300

    Update r.sh

--- scripts/r.sh
+++ scripts/r.sh
@@ -1,6 +1,8 @@
-modprobe dm-crypt
-cryptsetup luksOpen /dev/sda3 sda3
-mkdir /mnt/boot
-mount /dev/sda2 /mnt/boot
-mount /dev/mapper/sda3 /mnt
-arch-chroot /mnt /bin/bash
+systemctl enable netctl-ifplugd@`ip addr | grep "<" | grep -vi loopback | awk '{print $2}' | sed 's/://g'`
+
+#modprobe dm-crypt
+#cryptsetup luksOpen /dev/sda3 sda3
+#mkdir /mnt/boot
+#mount /dev/sda2 /mnt/boot
+#mount /dev/mapper/sda3 /mnt
+#arch-chroot /mnt /bin/bash
