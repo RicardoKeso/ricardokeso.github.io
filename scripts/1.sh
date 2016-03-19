76e209924aac65120f1ed202901e06580abf1630
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-19 08:44:49 -0300

    Update 1.sh

--- scripts/1.sh
+++ scripts/1.sh
@@ -1,15 +1,24 @@
-passwd # define a senha do root
+#passwd # define a senha do root
 
-pacman -Syyu
+#pacman -Syyu
 
-pacman -S grub --noconfirm ### instala o pacote do grub
-grub-install /dev/sda ### instala o grub no disco
-sed -i '/GRUB_TIMEOUT=/s/5/1/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
-sed -i '/GRUB_CMDLINE_LINUX=/s/""/"cryptdevice=/dev/sda3:sda3"/g' /etc/default/grub ###
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
+#pacman -S grub --noconfirm ### instala o pacote do grub
+#grub-install /dev/sda ### instala o grub no disco
+#sed -i '/GRUB_TIMEOUT=/s/5/1/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
+#sed -i '/GRUB_CMDLINE_LINUX=/s/""/"cryptdevice=/dev/sda3:sda3"/g' /etc/default/grub ###
+#echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 
-sed -i ':a;$!{N;ba;};s/\(.*\)filesystems/\1encrypt filesystems/' /etc/mkinitcpio.conf
-mkinitcpio -p linux ### compila a imagem do sistema
+#sed -i ':a;$!{N;ba;};s/\(.*\)filesystems/\1encrypt filesystems/' /etc/mkinitcpio.conf
+#mkinitcpio -p linux ### compila a imagem do sistema
 
-grub-install --recheck /dev/sda
-grub-mkconfig --output /boot/grub/grub.cfg ### configura o grub
+#grub-install --recheck /dev/sda
+#grub-mkconfig --output /boot/grub/grub.cfg ### configura o grub
+
+echo ""
+echo ""
+echo " * * * PROXIMOS COMANDOS * * * "
+echo "exit"
+echo "umount -R /mnt"
+echo "umount -R /mnt/boot"
+echo "cryptsetup close sda3"
+echo "systemctl reboot"
