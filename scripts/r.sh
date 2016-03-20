9f8010b7144592675695a139a1a81e20e2dfbc67
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-20 14:55:05 -0300

    Update r.sh

--- scripts/r.sh
+++ scripts/r.sh
@@ -7,25 +7,8 @@
 #mount /dev/mapper/sda3 /mnt
 #arch-chroot /mnt /bin/bash
 
-
-echo "#" > /root/.bashrc
-echo "# ~/.bashrc" >> /root/.bashrc
-echo "#" >> /root/.bashrc
-echo "" >> /root/.bashrc
-echo "alias ls='ls --color=auto'" >> /root/.bashrc
-echo "alias grep='grep --color=auto'" >> /root/.bashrc
-echo "" >> /root/.bashrc
-echo "# If not running interactively, don't do anything" >> /root/.bashrc
-echo "[[ $- != *i* ]] && return" >> /root/.bashrc
-echo "" >> /root/.bashrc
-cp /root/.bashrc /root/.bash_profile
-echo "if [ $UID -eq "0" ]; then" >> /root/.bashrc
-echo "  PS1='\[\033[1;37m\]rk\[\033[0;31m\] \$ \[\033[0m\]'" >> /root/.bashrc
-echo "else" >> /root/.bashrc
-echo "  PS1='\[\033[1;37m\]rk \$ \[\033[0m\]'" >> /root/.bashrc
-echo "fi" >> /root/.bashrc
-echo "PS1='\[\033[0;31m\] \$ \[\033[0m\]'" >> /root/.bash_profile
-sed -i 's/"# ~\/.bashrc"/"# ~\/.bash_profile"/g' /root/.bash_profile
+curl ricardokeso.github.io/scripts/rk_bashrc > .bashrc
+curl ricardokeso.github.io/scripts/rk_bash_profile > .bash_profile
 
 mv /home/ricardokeso/.bashrc /home/ricardokeso/.bashrc_original
 cp /root/.bashrc /home/ricardokeso/.bashrc
