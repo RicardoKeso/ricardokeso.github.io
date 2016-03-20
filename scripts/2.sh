4fad748b55ef0811a292347e714fda49f39451b9
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-20 13:58:14 -0300

    Update 2.sh

--- scripts/2.sh
+++ scripts/2.sh
@@ -20,6 +20,7 @@ pacman -S wget curl --noconfirm ### instala gerenciadores de download
 pacman -S bash-completion --noconfirm ### instala ferramenta para autocomplemento (tab) no terminal
 pacman -S mlocate --noconfirm ### instala as funções de pesquisa (updatedb, locate)
 pacman -S gnupg --noconfirm ### instala gnupg (GPG)
+pacman -S openssh --noconfirm ### instala openSSH
 
 #> PACOTES ESSENCIAIS GUI
 # pacman -S firefox --noconfirm ### instala o firefox
@@ -99,6 +100,29 @@ passwd ricardokeso ### altera a senha do usuário
 groupadd sudo ### cria o grupo sudo
 gpasswd -a ricarodkeso sudo ### adiciona o usuário ao grupo sudo
 sed -i '/# %sudo/s/#//g' /etc/sudoers ### descomenta a linha que permite superAcesso aos usuários do grupo sudo
+
+echo "#" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "# ~/.bashrc" >> /root/.bashrc
+echo "# ~/.bash_profile" >> /root/.bash_profile
+echo "#" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "alias ls='ls --color=auto'" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "alias grep='grep --color=auto'" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "# If not running interactively, don't do anything" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "[[ $- != *i* ]] && return" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "" >> /root/.bashrc; cat /root/.bashrc >> /root/.bash_profile
+echo "if [ $UID -eq "0" ]; then" >> /root/.bashrc
+echo "  PS1='\[\033[1;37m\]rk\[\033[0;31m\] \$ \[\033[0m\]'" >> /root/.bashrc
+echo "  PS1='\[\033[0;31m\] \$ \[\033[0m\]'" >> /root/.bash_profile
+echo "else" >> /root/.bashrc
+echo "	PS1='\[\033[1;37m\]rk \$ \[\033[0m\]'" >> /root/.bashrc
+echo "fi" >> /root/.bashrc
+
+mv /home/ricardokeso/.bashrc /home/ricardokeso/.bashrc_original
+cp /root/.bashrc /home/ricardokeso/
+chown ricardokeso:ricardokeso /home/ricardokeso/.bashrc
+chmod 644 /home/ricardokeso/.bashrc
 
 su ricardokeso
 echo "pinentry-program /usr/bin/pinentry-curses" > ~/.gnupg/gpg-agent.conf
