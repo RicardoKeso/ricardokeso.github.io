6c12c182fe892bf9ba2fbb9e62a6b2dc3ca66495
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-21 15:59:16 -0300

    Update 2_grub.sh

--- scripts/2_grub.sh
+++ scripts/2_grub.sh
@@ -1,9 +1,9 @@
 echo " * * * * * ALTERAR SENHA ROOT * * * * * "
 passwd # define a senha do root
 
-pacman -Syyu
+#pacman -Syyu (verificar necessidade)
 
-pacman -S grub --noconfirm ### instala o pacote do grub
+#pacman -S grub --noconfirm ### instala o pacote do grub (confirmar se o grub não vem no grupo base)
 grub-install /dev/sda ### instala o grub no disco
 sed -i '/GRUB_TIMEOUT=/s/5/1/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
 sed -i '/GRUB_CMDLINE_LINUX=/s/""/"cryptdevice=\/dev\/sda3:sda3"/g' /etc/default/grub ###
