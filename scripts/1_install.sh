7e880e95b3fbf35bc67bbeb48f70a9af8dc252ec
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-21 14:54:08 -0300

    Update 1_install.sh

--- scripts/1_install.sh
+++ scripts/1_install.sh
@@ -1,5 +1,6 @@
 clear
 echo " * * * * * CRIANDO PARTICOES * * * * * "
+echo ""
 parted -s /dev/sda mklabel msdos
 parted -s /dev/sda mkpart primary linux-swap 1MiB 257MiB ### cria uma particao de SWAP de 256MB
 parted -s /dev/sda mkpart primary ext4 257MiB 321MiB ### cria particao de BOOT 64MB
@@ -9,26 +10,32 @@ mkswap /dev/sda1  ### formata a partição de swap
 swapon /dev/sda1  ### ativa a partição de swap
 echo ""
 echo " * * * * * CRIPTOGRAFANDO PARTICAO RAIZ * * * * * "
+echo ""
 modprobe dm-crypt ### carrega o módulo de criptografia
 cryptsetup -c aes-xts-plain64 -y -s 512 luksFormat /dev/sda3 ### criptografa a partição selecionada
 cryptsetup luksOpen /dev/sda3 sda3 ### abre a partição criptografada e atribui um mapeamento
 echo ""
 echo " * * * * * FORMATANDO PARTICOES * * * * * "
+echo ""
 mkfs.ext4 /dev/sda2 ### cria o sistema de arquivos na partição de boot que não pode ser criptografada
 mkfs.ext4 /dev/mapper/sda3 ### cria o sistema de arquivos através do mapeamento para a partição criptografada
 echo ""
 echo " * * * * * MONTANDO PARTICOES * * * * * "
+echo ""
 mkdir /mnt/boot ### para criar a raiz (/mnt) e o boot (/mnt/boot)
 mount /dev/sda2 /mnt/boot ### monta a partição de boot
 mount /dev/mapper/sda3 /mnt ### monta a partição raiz
 echo ""
 echo " * * * * * INSTALANDO SISTEMA * * * * * "
+echo ""
 pacstrap /mnt base base-devel grub-bios ### instala o sistema na partição raiz
 echo ""
 echo " * * * * * GERANDO A TABELA DE ARQUIVOS DE SISTEMAS * * * * * "
+echo ""
 genfstab -U -p /mnt >> /mnt/etc/fstab ### cria a tabela de discos
 echo ""
 echo " * * * * * BAIXANDO SCRIPTS DE POS INSTALACAO * * * * * "
+echo ""
 curl ricardokeso.github.io/scripts/2_grub.sh > /mnt/tmp/2_grub.sh
 chmod +x /mnt/tmp/2_grub.sh
 curl ricardokeso.github.io/scripts/3_config.sh >/mnt/tmp/3_config.sh
