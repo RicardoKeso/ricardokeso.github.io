f5e58143d361178b91565bc3facd0e0e462ce606
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-21 16:38:47 -0300

    Update 1_install.sh

--- scripts/1_install.sh
+++ scripts/1_install.sh
@@ -36,10 +36,8 @@ genfstab -U -p /mnt >> /mnt/etc/fstab ### cria a tabela de discos
 echo ""
 echo " * * * * * BAIXANDO SCRIPTS DE POS INSTALACAO * * * * * "
 echo ""
-curl ricardokeso.github.io/scripts/2_grub.sh > /tmp/2_grub.sh
-chmod +x /tmp/2_grub.sh
-curl ricardokeso.github.io/scripts/3_config.sh > /tmp/3_config.sh
-chmod +x /tmp/3_config.sh
+curl ricardokeso.github.io/scripts/2_grub.sh > /mnt/root/2_grub.sh
+chmod +x /mnt/root/2_grub.sh
 echo ""
-echo " * * * * * DIGITE: /tmp/2_grub.sh* * * * * "
+echo " * * * * * DIGITE: /mnt/root/2_grub.sh* * * * * "
 arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
