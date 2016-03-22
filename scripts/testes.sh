a1371dc7d6c72c71c47f26426e4ebd5022d4eb0b
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-22 15:04:37 -0300

    Update testes.sh

--- scripts/testes.sh
+++ scripts/testes.sh
@@ -7,26 +7,8 @@
 #mount /dev/mapper/sda3 /mnt
 #arch-chroot /mnt /bin/bash
 
-echo ""
-echo " * * * * * ALTERAR SENHA ROOT * * * * * "
-echo ""
-passwd # define a senha do root
+teste(){
+  echo "funciona"
+}
 
-echo ""
-echo " * * * * * SINCRONIZANDO E ATUALIZANDO PACOTES * * * * * "
-echo ""
-pacman -Syyu ### sincronizacao e atualizacao total
-
-echo ""
-echo " * * * * * INSTALANDO E CONFIGURANDO O GRUB * * * * * "
-echo ""
-#pacman -S grub --noconfirm ### instala o pacote do grub (confirmar se o grub não vem no grupo base)
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-sed -i '/GRUB_TIMEOUT=/s/5/1/g' /etc/default/grub ### reduz o tempo da seleção de 5 para 2 segundos
-sed -i '/GRUB_CMDLINE_LINUX=/s/""/"cryptdevice=\/dev\/sda3:sda3"/g' /etc/default/grub ###
-#sed -i '/GRUB_GFXMODE=/s/"auto"/"1024x768"/g' /etc/default/grub ### mudar resolucao do terminal(utilizado em vc sem GUI)
-sed -i ':a;$!{N;ba;};s/\(.*\)filesystems/\1encrypt filesystems/' /etc/mkinitcpio.conf
-grub-install /dev/sda ### instala o grub no disco
-mkinitcpio -p linux ### compila a imagem do sistema
-grub-install --recheck /dev/sda
-grub-mkconfig --output /boot/grub/grub.cfg ### configura o grub
+#teste
