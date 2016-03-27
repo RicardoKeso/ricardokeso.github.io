c621ca61ef4bc5701a92145c44a1f0cdb6228956
RicardoKeso <ricardo_keso@hotmail.com>
2016-03-27 14:16:07 -0300

    Update 2_config.sh

--- scripts/2_config.sh
+++ scripts/2_config.sh
@@ -49,6 +49,7 @@ essenciaisGUI(){
   pacman -S firefox --noconfirm ### instala o firefox
   pacman -S pcmanfm --noconfirm ### gerenciado de arquivos grafico
   pacman -S feh --noconfirm ### visualizador de imagens (serve para gerir o wallpaper do desktop)
+  pacman -S scrot --noconfirm ### ferramenta de printscreen
 }
 
 notebook(){
