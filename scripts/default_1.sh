5d647ee49caccb3f2a0bca089ff16dc4005e76d1
RicardoKeso <ricardo_keso@hotmail.com>
2016-04-14 11:50:14 -0300

    Update default_1.sh

--- scripts/default_1.sh
+++ scripts/default_1.sh
@@ -4,7 +4,7 @@ echo ""
 parted -s /dev/sda mklabel msdos
 parted -s /dev/sda mkpart primary linux-swap 1MiB 257MiB ### cria uma particao de SWAP de 256MB
 parted -s /dev/sda mkpart primary ext4 257MiB 321MiB ### cria particao de BOOT 64MB
-parted -s /dev/sda mkpart primary ext4 321MiB 4417MB ### cria particao raiz com 4GB
+parted -s /dev/sda mkpart primary ext4 321MiB 100% ### cria particao raiz com 4GB (4417 = 3.8) ()
 parted -s /dev/sda set 2 boot on ### 
 mkswap /dev/sda1  ### formata a partição de swap
 swapon /dev/sda1  ### ativa a partição de swap
