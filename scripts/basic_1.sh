a09b4153c4565aa91014a91f2add9570472b1fd9
RicardoKeso <ricardo_keso@hotmail.com>
2016-04-28 11:12:58 -0300

    Update basic_1.sh

--- scripts/basic_1.sh
+++ scripts/basic_1.sh
@@ -1,42 +1,18 @@
-criarParticoes(){
-	clear
-	echo " * * * * * CRIANDO PARTICOES * * * * * "
-	echo ""
-	parted -s /dev/sda mklabel msdos
-	parted -s /dev/sda mkpart primary linux-swap 1MiB 129MiB ### cria uma particao de SWAP de 128MB
-	parted -s /dev/sda mkpart primary ext4 129MiB 193MiB ### cria particao de BOOT 64MB
-	parted -s /dev/sda mkpart primary ext4 193MiB 832MiB ### cria particao raiz
-	parted -s /dev/sda set 2 boot on
-	mkswap /dev/sda1
-	swapon /dev/sda1
-}
+parted -s /dev/sda -- mklabel msdos
+parted -s /dev/sda mkpart primary linux-swap 1MiB 129MiB	# sda1 swap
+parted -s /dev/sda mkpart primary ext4 129MiB 832MiB		# sda2 raiz
 
-formatarParticoes(){
-	echo ""
-	echo " * * * * * FORMATANDO PARTICOES * * * * * "
-	echo ""
-	mkfs.ext4 /dev/sda2
-	mkfs.ext4 /dev/sda3
-}
+mkswap /dev/sda1
+swapon /dev/sda1
 
-montarParticoes(){	
-	echo ""
-	echo " * * * * * MONTANDO PARTICOES * * * * * "
-	echo ""
-	mkdir /mnt/boot ### para criar a raiz (/mnt) e o boot (/mnt/boot)
-	mount /dev/sda2 /mnt/boot ### monta a partição de boot
-	mount /dev/sda3 /mnt ### monta a partição raiz
-}
+mkfs.ext4 /dev/sda2
 
-instalarSistema(){	
-	echo ""
-	echo " * * * * * INSTALANDO SISTEMA * * * * * "
-	echo ""
-	pacstrap /mnt base
-	echo ""
-	echo " * * * * * GERANDO A TABELA DE ARQUIVOS DE SISTEMAS * * * * * "
-	echo ""
-	genfstab -U -p /mnt >> /mnt/etc/fstab
+parted -s /dev/sda set 2 boot on
+
+mount /dev/sda2 /mnt
+
+pacstrap /mnt base
+genfstab -p /mnt >> /mnt/etc/fstab
 
 scriptsPosInstalacao(){	
 	echo ""
@@ -46,11 +22,7 @@ scriptsPosInstalacao(){
 	chmod +x /mnt/root/basic_2.sh
 	echo ""
 	echo " * * * * * DIGITE: /root/basic_2.sh* * * * * "
-	arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
+	arch-chroot /mnt ### retorna para o sistema instalado
 }
 
-criarParticoes
-formatarParticoes
-montarParticoes
-instalarSistema
 #scriptsPosInstalacao
