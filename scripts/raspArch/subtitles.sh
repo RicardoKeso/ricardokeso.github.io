93f9b2a315878598a9942e1a850398465120e89c
RicardoKeso <ricardo_keso@hotmail.com>
2016-05-26 00:57:33 -0300

    Update subtitles.sh

--- scripts/raspArch/subtitles.sh
+++ scripts/raspArch/subtitles.sh
@@ -3,29 +3,38 @@
 clear
 echo ""
 
-echo "Title: "$1
+mkdir -p Subtitles
+
+echo "Title: "$1 | awk '{print toupper($0)}'
 titulo=`echo $1 | awk '{print tolower($0)}'`
-#echo $titulo
+echo $titulo > searchLog
 
 tituloHtml=`echo $titulo | sed "s/ /%20/g"`
-#echo "titulo HTML: "$tituloHtml
+echo "titulo HTML: "$tituloHtml >> searchLog
 
-imdbID=`curl -s "http://www.omdbapi.com/?t=$tituloHtml&plot=full&r=json" | sed "s/imdbID\":\"/\|/" | cut -d '|' -f2 | cut -d '"' -f1`
-echo "IMDb ID: "$imdbID
+#imdbID=`curl -s "http://www.omdbapi.com/?t=$tituloHtml&plot=full&r=json" | sed "s/imdbID\":\"/\|/" | cut -d '|' -f2 | cut -d '"' -f1`
+imdbID_ok=`curl -s http://www.yifysubtitles.com/search?q=$tituloHtml | grep "movie-imdb/tt" | grep "movie-imdb\/" | sed "s/movie-imdb\//|/" | cut -d '|' -f2 | cut -d '"' -f1`
 
-codSub=`curl -s http://www.yifysubtitles.com/movie-imdb/$imdbID | grep "brazilian-portuguese" | sed "s/brazilian-portuguese-yify-/|/" | cut -d '|' -f2 | cut -d '"' -f1`
-#echo "Cod Sub: "$codSub
+echo "IMDb ID: "$imdbID_ok >> searchLog
+echo "IMDb ID: "$imdbID_ok
 
-arquivo=`echo $titulo-brazilian-portuguese-yify-$codSub.zip | sed 's/ /_/g'`
-#echo "Arquivo: "$arquivo
+codSub=`curl -s http://www.yifysubtitles.com/movie-imdb/$imdbID_ok | grep "brazilian-portuguese" | sed "s/brazilian-portuguese-yify-/|/" | cut -d '|' -f2 | cut -d '"' -f1`
+echo "Cod Sub: "$codSub >> searchLog
 
-arquivoHtml=`echo $arquivo | sed 's/_/-/g'`
-#echo $arquivoHtml
+if [ "$codSub" != "" ]; then
+	arquivo=`echo $titulo-brazilian-portuguese-yify-$codSub.zip | sed 's/ /_/g'`
+	echo "Arquivo: "$arquivo >> searchLog
 
-wget -q "http://www.yifysubtitles.com/subtitle/$arquivoHtml"
+	arquivoHtml=`echo $arquivo | sed 's/_/-/g'`
 
-#unzip $arquivoHtml *.srt
+	wget -q "http://www.yifysubtitles.com/subtitle/$arquivoHtml"
 
-#rm -f $arquivoHtml
+	unzip -q $arquivoHtml -d Subtitles/
+
+	rm -f $arquivoHtml
+else
+	echo ""
+	echo "Error: codSub empty"
+fi
 
 echo ""
