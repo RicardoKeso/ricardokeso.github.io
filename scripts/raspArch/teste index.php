58134dd48c69f4d1aeabe9b6358b898cf470cb8d
RicardoKeso <ricardo_keso@hotmail.com>
2016-06-02 18:11:29 -0300

    Create teste index.php

---
+++ scripts/raspArch/teste index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <meta charset="UTF-8">
+        <title>titles</title>
+        <script https://code.jquery.com/jquery-2.2.3.min.js>
+        </script>
+        </head>
+        <body>
+                <form action="sub.php" method="post">
+                        Title: <input type="text" name="title">
+                        <input type="submit" value="ok">
+                </form>
+                <?php
+                        header("Refresh:10");
+
+                        echo "<div>";
+                        $path = "files/";
+                        $diretorio = dir($path);
+                        $cont = 0;
+
+                        echo "<br/><strong>Files</strong><br/><br/>";
+
+                        while($arquivo = $diretorio -> read()){
+                                if (($arquivo != "..") && ($arquivo != ".") && ($arquivo != "sub.sh") && ($arquivo != "sub.php")){
+                                        echo $arquivo."<br />";
+                                        $cont++;
+                                }
+                        }
+
+                        if ($cont == 0) {
+                                echo "empty";
+                        }
+
+                        $diretorio -> close();
+                        echo "</div><br><hr><br>";
+                        echo $_POST["title"];
+                        echo shell_exec('mkdir teste');
+                ?>
+        </body>
+</html>
