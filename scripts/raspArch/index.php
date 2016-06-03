0a7c2cb0c2a0c6eeedf9d29d5d807d63702b41ff
RicardoKeso <ricardo_keso@hotmail.com>
2016-06-03 17:37:09 -0300

    Update index.php

--- scripts/raspArch/index.php
+++ scripts/raspArch/index.php
@@ -2,25 +2,29 @@
 <html>
         <head>
         <meta charset="UTF-8">
-        <title>ArchRPi</title>
+        <title>titles</title>
         <script https://code.jquery.com/jquery-2.2.3.min.js>
         </script>
         </head>
-
         <body>
+                <form action="files/sub.php" method="post">
+                        Title: <input type="text" name="title">
+                        <input type="submit" value="ok">
+                </form>
                 <?php
-                        header("Refresh:10");
+#                       header("Refresh:10");
 
                         echo "<div>";
-                        $path = "torrents/";
+                        $path = "files/";
                         $diretorio = dir($path);
                         $cont = 0;
 
-                        echo "<strong>ADDED</strong><br/><br/>";
+                        echo "<br/><strong>Files</strong><br/><br/>";
+
                         while($arquivo = $diretorio -> read()){
-                                if (($arquivo != "..") && ($arquivo != ".")) {
-                                        echo $arquivo."<br />";
-                                        $cont++;
+                                if (($arquivo != "..") && ($arquivo != ".") && ($arquivo != "sub.sh") && ($arquivo != "sub.php")){
+                        echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br />";
+                        $cont++;
                                 }
                         }
 
@@ -30,78 +34,7 @@
 
                         $diretorio -> close();
                         echo "</div><br><hr><br>";
-
-                        //------------------------------------------------------------------------
-
-                        echo "<div>";
-                        $path = "incomplete/";
-                        $diretorio = dir($path);
-                        $cont = 0;
-
-                        echo "<strong>INCOMPLETE</strong><br/><br/>";
-                        while($arquivo = $diretorio -> read()){
-                                if (($arquivo != "..") && ($arquivo != ".")) {
-                                        echo $arquivo."<br />";
-                                        $cont++;
-                                }
-                        }
-
-                        if ($cont == 0) {
-                                echo "empty";
-                        }
-
-                        $diretorio -> close();
-                        echo "</div><br><hr><br>";
-
-                        //------------------------------------------------------------------------
-
-                        echo "<div>";
-                        $cont=0;
-                        $path = "storage/";
-                        $diretorio = dir($path);
-
-                        echo "<strong>FINISHED</strong><br/><br>";
-                        while($arquivo = $diretorio -> read()){
-                                if (($arquivo != "..") && ($arquivo != ".")) {
-                                        echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br />";
-                                        $cont++;
-                                }
-                        }
-
-                        if ($cont == 0) {
-                                echo "empty";
-                        }
-
-                        $diretorio -> close();
-                        echo "</div><br><hr><br>";
-
-                        //------------------------------------------------------------------------
-
-                        echo "<div>";
-
-                        echo "<b>";
-                        echo shell_exec('date');
-                        echo "</b>";
-
-                        $ponteiro = fopen("dados", "r");
-
-                        while (!feof ($ponteiro)) {
-                                echo $linha."<br>";
-                                $linha = fgets($ponteiro, 4096);
-                        }
-                        fclose($ponteiro);
-
-                        echo shell_exec('awk \'{printf "Temp: %3.1f C\n", $1/1000}\' /sys/class/thermal/thermal_zone0/temp');
-                        echo "<br>";
-                        echo shell_exec('curl -s http://checkip.amazonaws.com');
-                        echo " - public<br>";
-                        echo shell_exec('ip a | grep "inet " | grep -v "127.0.0.1" | awk \'{print $2 " - " $7}\' | sed \':a;$!N;s/\n/<br>/g;ta\'');
-                        echo "<br><br>";
-
-                        echo "</div><hr><br>";
+                        echo $_POST["title"];
                 ?>
-                <div style="color: white;">
-                        kingbradley - S1gmUndF!
-                </div>
         </body>
 </html>
