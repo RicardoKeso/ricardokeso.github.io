d4cca2fc9341fb9dae0c211550159590434ffa58
RicardoKeso <ricardo_keso@hotmail.com>
2016-06-04 15:15:49 -0300

    Update TESTE index.php

--- scripts/raspArch/TESTE index.php
+++ scripts/raspArch/TESTE index.php
@@ -7,29 +7,49 @@
         </script>
         </head>
         <body>
-                <form action="files/sub.php" method="post">
+                <form action="files/whait.php" method="post">
                         Title: <input type="text" name="title">
+                        <!--input type="checkbox" name="files[]" value="1" checked="true">IMDB Data |
+                        <input type="checkbox" name="files[]" value="2" checked="true">Poster |
+                        <input type="checkbox" name="files[]" value="4" checked="true">Srt |-->
+                        <input type="checkbox" name="torrent" value="8" checked="true">Torrent
+                        </br>
                         <input type="submit" value="ok">
+                        <br><br>
+                        <hr>
+
                 </form>
+                <!-- #Region PHP -->
                 <?php
 #                       header("Refresh:10");
+/*
+                        $sum = 0;
+                        foreach($_POST['files'] as $fileVal) {
+                                $sum += $fileVal;
+                        }
+                        echo decbin($sum);
+*/
                         echo "<div>";
                         $path = "files/";
                         $diretorio = dir($path);
                         $cont = 0;
+
                         echo "<br/><strong>Files</strong><br/><br/>";
+
                         while($arquivo = $diretorio -> read()){
                                 if (($arquivo != "..") && ($arquivo != ".") && ($arquivo != "sub.sh") && ($arquivo != "sub.php")){
                         echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br />";
                         $cont++;
                                 }
                         }
+
                         if ($cont == 0) {
                                 echo "empty";
                         }
+
                         $diretorio -> close();
                         echo "</div><br><hr><br>";
-                        echo $_POST["title"];
                 ?>
+                <!-- #EndRegion -->
         </body>
 </html>
