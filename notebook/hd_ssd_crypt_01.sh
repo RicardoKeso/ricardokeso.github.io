0e57f826b27acea0f4c2a0ff99f5f74bd6bd43e5
RicardoKeso <ricardo_keso@hotmail.com>
2016-06-22 12:35:01 -0300

    Update hd_ssd_crypt_01.sh

--- notebook/hd_ssd_crypt_01.sh
+++ notebook/hd_ssd_crypt_01.sh
@@ -1,37 +1,79 @@
 
-parted -s /dev/sda mklabel msdos
-parted -s /dev/sda mkpart primary ext4 1MiB 513MiB
-parted -s /dev/sda mkpart primary ext4 513MiB 100%
-parted -s /dev/sda set 1 boot on ### 
-parted -s /dev/sda set 2 lvm on ### 
+Particionamento_GPT(){
+	parted -s /dev/sda mklabel gpt
+	parted -s /dev/sda mkpart primary ext4 1MiB 513MiB
+	parted -s /dev/sda mkpart primary ext4 513MiB 100%
+	parted -s /dev/sda set 1 boot on 
+	parted -s /dev/sda set 2 lvm on 
+}
 
-modprobe dm-crypt
-cryptsetup -c aes-xts-plain64 -y -s 512 luksFormat /dev/sda2
-cryptsetup luksOpen /dev/sda2 lvm
+Criptografia(){
+	modprobe dm-crypt
+	cryptsetup -c aes-xts-plain64 -y -s 512 luksFormat /dev/sda2
+	cryptsetup luksOpen /dev/sda2 lvm
+}
 
-pvcreate /dev/mapper/lvm
-vgcreate main /dev/mapper/lvm
+LVM(){
+	pvcreate /dev/mapper/lvm
+	vgcreate main /dev/mapper/lvm
 
-lvcreate -L 2GB -n swap main
-lvcreate -L 2GB -n root main
-lvcreate -l 100%FREE -n home main
+	lvcreate -L 2GB -n swap main
+	lvcreate -L 2GB -n root main
+	lvcreate -l 100%FREE -n home main
 
-mkswap /dev/mapper/main-swap
-mkfs.ext4 /dev/mapper/main-root
-mkfs.ext4 /dev/mapper/main-home
+	mkswap /dev/mapper/main-swap
+	mkfs.ext4 /dev/mapper/main-root
+	mkfs.ext4 /dev/mapper/main-home
+}
 
-mount /dev/mapper/main-root /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir /mnt/home
-mount /dev/mapper/main-home /mnt/home
+Montagem(){
+	mount /dev/mapper/main-root /mnt
+	mkdir /mnt/boot
+	mount /dev/sda1 /mnt/boot
+	mkdir /mnt/home
+	mount /dev/mapper/main-home /mnt/home
+}
 
-pacstrap /mnt base base-devel grub-bios
+LinguagemRegiao(){
+	sed -i '/en_US.UTF-8 UTF-8/s/#//g' /etc/locale.gen
+	locale-gen
+}
 
-genfstab -U -p /mnt >> /mnt/etc/fstab
+InstalacaoBase(){
+	pacstrap /mnt base
+}
 
-curl www.ricardokeso.com/scripts/2_config.sh > /mnt/root/2_config.sh
-chmod +x /mnt/root/2_config.sh
-echo ""
-echo " * * * * * DIGITE: /root/2_config.sh* * * * * "
-arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
+InstalacaoBase-Devel(){
+	pacstrap /mnt base-devel
+}
+
+InstalacaoGrub-Bios(){
+	pacstrap /mnt grub-bios
+}
+
+InstalacaoSistema(){
+	InstalacaoBase
+	InstalacaoBase-Devel
+	InstalacaoGrub-Bios
+}
+
+GeracaoFSTAB(){
+	genfstab -U -p /mnt >> /mnt/etc/fstab
+}
+
+ScriptPosInstalacao(){
+	curl www.ricardokeso.com/scripts/2_config.sh > /mnt/root/2_config.sh
+	chmod +x /mnt/root/2_config.sh
+	echo ""
+	echo " * * * * * DIGITE: /root/2_config.sh* * * * * "
+	arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
+}
+
+#Particionamento_GPT
+#Criptografia
+#LVM
+#Montagem
+#LinguagemRegiao
+#InstalacaoSistema
+#GeracaoFSTAB
+#ScriptPosInstalacao
