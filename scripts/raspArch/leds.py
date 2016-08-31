3e26c8ad532045ef1cd99e72b2b9bd16512ee07f
RicardoKeso <ricardo_keso@hotmail.com>
2016-08-31 14:52:53 -0300

    Create leds.py

---
+++ scripts/raspArch/leds.py
@@ -0,0 +1,43 @@
+#!/bin/python2.7
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+cor = sys.argv[1]
+pinLedRed = 15
+pinLedGreen = 13
+pinLedBlue = 11
+
+GPIO.setup(pinLedRed, GPIO.OUT)
+GPIO.setup(pinLedGreen, GPIO.OUT)
+GPIO.setup(pinLedBlue, GPIO.OUT)
+
+def AjusteCores (red, green, blue, tempo):
+	GPIO.output(pinLedRed, red)
+	GPIO.output(pinLedGreen, green)
+	GPIO.output(pinLedBlue, blue)
+	time.sleep(tempo)
+
+def Cores (cor):
+	if (cor == "white"):
+		AjusteCores (1, 1, 1, 0.0001)
+	elif (cor == "red"):
+		AjusteCores (1, 0, 0, 0.0001)
+	elif (cor == "green"):
+		AjusteCores (0, 1, 0, 0.0001)
+	elif (cor == "blue"):
+		AjusteCores (0, 0, 1, 0.0001)
+	elif (cor == "yellow"):
+		AjusteCores (1, 1, 0, 0.0001)
+	elif (cor == "magenta"):
+		AjusteCores (1, 0, 1, 0.0001)
+	elif (cor == "cyan"):
+		AjusteCores (0, 1, 1, 0.0001)
+	else:
+		AjusteCores (0, 0, 0, 0.0001)
+	
+Cores (cor)
+	
