df697643d5507a33fef9be822d50d8a00369bdaa
Ricardo Melo <120052617+RicardoMelo275@users.noreply.github.com>
2022-12-15 11:48:45 -0300

    20221215

--- scripts/Linux/padrao.sh
+++ scripts/Linux/padrao.sh
@@ -3,15 +3,15 @@ parted -s /dev/sda mkpart primary linux-swap 1MiB 1024MiB
 parted -s /dev/sda mkpart primary ext4 1024MiB 100%
 
 mkswap /dev/sda1
-mkfs.ext4 /dev/sda2
-
 swapon /dev/sda1
+
+mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 
 pacstrap -K /mnt base linux
 #genfstab -U /mnt >> /mnt/etc/fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+#arch-chroot /mnt
 
 #ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 #hwclock --systohc
