866d242c075b1cb5583c2cbc500fd63d44630a5c
Ricardo Melo <120052617+RicardoMelo275@users.noreply.github.com>
2022-12-15 14:13:01 -0300

    20221215

--- scripts/Linux/padrao.sh
+++ scripts/Linux/padrao.sh
@@ -1,17 +1,15 @@
 parted -s /dev/sda mklabel msdos
-parted -s /dev/sda mkpart primary linux-swap 1MiB 1024MiB
-parted -s /dev/sda mkpart primary ext4 1024MiB 100%
-
-mkswap /dev/sda1
-swapon /dev/sda1
-
-mkfs.ext4 /dev/sda2
-parted -s /dev/sda set 2 boot on
-mount /dev/sda2 /mnt
-
-pacstrap /mnt base
-genfstab -p /mnt >> /mnt/etc/fstab
-
-# pacstrap -K /mnt base #linux
-# genfstab -U /mnt >> /mnt/etc/fstab
-# genfstab -U -p /mnt >> /mnt/etc/fstab
+parted -s /dev/sda mkpart primary linux-swap 1MiB 512MiB ### cria uma particao de SWAP de 256MB
+parted -s /dev/sda mkpart primary ext4 512MiB 640MiB ### cria particao de BOOT 64MB
+parted -s /dev/sda mkpart primary ext4 640MiB 100% ### cria particao raiz com 4GB (4417 = 3.8) ()
+parted -s /dev/sda set 2 boot on ### 
+mkswap /dev/sda1  ### formata a partição de swap
+swapon /dev/sda1  ### ativa a partição de swap
+mkfs.ext4 /dev/sda2 ### cria o sistema de arquivos na partição de boot que não pode ser criptografada
+mkfs.ext4 /dev/sda3 ### cria o sistema de arquivos
+mkdir /mnt/boot ### para criar a raiz (/mnt) e o boot (/mnt/boot)
+mount /dev/sda2 /mnt/boot ### monta a partição de boot
+mount /dev/sda3 /mnt ### monta a partição raiz
+pacstrap /mnt base grub-bios
+genfstab -U -p /mnt >> /mnt/etc/fstab ### cria a tabela de discos
+arch-chroot /mnt /bin/bash ### retorna para o sistema instalado
